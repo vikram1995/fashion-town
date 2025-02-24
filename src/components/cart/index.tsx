53d32ce0aa614cb2ad175834effb36ce6ea2b7de
Vikram <vikram.1rn14ec170@gmail.com>
2025-02-24 15:12:22 +0530

    feat(nav) : add basic nav menu and search

---
+++ src/components/cart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { ShoppingBagIcon } from '@heroicons/react/24/outline'
+
+function Cart() {
+    return (
+        <ShoppingBagIcon className="size-6 cursor-pointer" />
+
+    )
+}
+
+export default Cart
